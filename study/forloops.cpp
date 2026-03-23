5fa2af3fe9bc5e94d05f0e62d6f10eda2089b964
Arman4102 <pinil1862@gmail.com>
2026-03-23 18:41:09 +0700

    for loops, is like js

---
+++ study/forloops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // like for loops in javascript
+    // start, condition/stop, increment/decrement
+    // for (int i = 1; i <= 3; i++) {
+    //     cout << "Hello " << i << endl;
+    // }
+
+    for (int i = 0; i <= 10; i+=2) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
